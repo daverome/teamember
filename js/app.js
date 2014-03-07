48e7c588e28ae7281324cac8e40e6145f23d0fe5
Dave Romero <david@freshtilledsoil.com>
2014-03-07 10:29:28 -0500

    Inital view templates

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  //this.resource('add', {path: '/add'});
+  this.route('add');
+});
+
+App.AddRoute = Ember.Route.extend({
+
 });
 
 App.IndexRoute = Ember.Route.extend({
